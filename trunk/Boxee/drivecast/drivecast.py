61305b5b69701da905e7be7a506b5db0edc2758f
Francesco.Alisetta <Francesco.Alisetta@a40d3fb1-556d-6172-e523-d82c01862e03>
2011-05-31 10:09:46 +0000

    Drivecast web-TV plugins

--- trunk/Boxee/drivecast/drivecast.py
+++ trunk/Boxee/drivecast/drivecast.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-import sys, urllib, urllib2, unicodedata
+import sys, urllib, urllib2, unicodedata, mc
 from simplejson import load
 from base64 import b64encode
 from xml.dom.minidom import parseString
@@ -8,9 +8,30 @@ from xml.dom.minidom import parseString
 # plugin constants
 __url__ = 'https://drivecast.eu'
 __api_key__ = "QXz8eEdSeKDoBvg4CzX4gU3n6MnliMZ9"
-__settings__ = xbmcaddon.Addon(id='plugin.video.drivecast')
-__language__ = __settings__.getLocalizedString
-__thumbnails__ = __settings__.getAddonInfo('path')+"/thumbnails/"
+#__thumbnails__ = __settings__.getAddonInfo('path')+"/thumbnails/"
+
+config = mc.GetApp().GetLocalConfig()
+
+def log():
+	username = config.GetValue("username")	#USERNAME
+	password = config.GetValue("password")	#PASSWORD
+	if username=="" or password=="":
+		return login()
+	else:
+		aut = b64encode(username+':'+password)	#AUTENTICAZIONE IN BASE64
+		return read_RSS(read_resource(__url__, aut, "feed"))
+
+def login():
+	username = config.GetValue("username")	#USERNAME
+	password = config.GetValue("password")	#PASSWORD
+	aut = b64encode(username+':'+password)	#AUTENTICAZIONE IN BASE64
+	try:
+		rss = read_RSS(read_resource(__url__, aut, "feed"))
+		return rss
+	except:
+		username = mc.ShowDialogKeyboard("Enter Username", "")	#USERNAME
+		password = mc.ShowDialogKeyboard("Enter Password", "", True)	#PASSWORD
+		return None
 
 #========================================================================================
 #	LETTURA LIBRERIA UTENTE														<<<OK>>>>
@@ -19,9 +40,9 @@ __thumbnails__ = __settings__.getAddonInfo('path')+"/thumbnails/"
 #========================================================================================
 #	API																			<<<OK>>>>
 #========================================================================================
-def read_resource(url, aut, resource):
+def read_resource(aut, resource):
 	try:
-		request = urllib2.Request(url+"/api/2.0/"+resource)
+		request = urllib2.Request(__url__+"/api/2.0/"+resource+"?fmt=xml")
 		opener = urllib2.build_opener()
 		request.add_header('User-Agent','DriveCast apikey='+__api_key__)					# aggiungo apikey all'header
 		request.add_header('Authorization','Basic '+aut)									# aggiungo dati login utente all'header
